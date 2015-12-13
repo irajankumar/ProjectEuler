a6f3141397771703eea50eb074fd67239c678b03
rkking <rkking@live.com>
2015-12-14 00:51:46 +0530

    added 17 in CPP

---
+++ Project euler 17/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<inttypes.h>
+using namespace std;
+uint64_t LetterCounter(int no)
+{
+
+    int dict[100]={}
+}
+int main()
+{
+    cout << "Okay lets count all the letters till 1000" << endl;
+    return 0;
+}
